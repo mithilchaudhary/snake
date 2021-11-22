8120bdc6127f119a9e1b2cf739360a9438f16518
Mithil Chaudhary <79639948+mithilchaudhary@users.noreply.github.com>
2021-11-22 23:01:55 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+lf.body[0].x >= cell_number or self.body[0].x < 0:
+        #     self.body[0].x = 0
+        # if self.body[0].y >= cell_number or self.body[0].y < 0:
+        #     print('out of screen y')
